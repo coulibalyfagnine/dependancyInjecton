060192d5f4eca2af0835bda2fd5d2b4727964f84
Fagnine <coulibalyfagnine@gmail.com>
2021-02-17 10:11:39 +0100

    first commit

---
+++ src/test/java/com/alas/dependancyInjecton/DependancyInjectonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alas.dependancyInjecton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DependancyInjectonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
