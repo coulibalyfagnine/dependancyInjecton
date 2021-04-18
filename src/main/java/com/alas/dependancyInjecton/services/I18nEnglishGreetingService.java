f9486ef1b6259e7c82e0cf224f355efd4eb152a7
Fagnine <coulibalyfagnine@gmail.com>
2021-04-18 12:06:18 +0200

    set one specific service implementation as default, just in case no spring.profiles.actives is not set

--- src/main/java/com/alas/dependancyInjecton/services/I18nEnglishGreetingService.java
+++ src/main/java/com/alas/dependancyInjecton/services/I18nEnglishGreetingService.java
@@ -3,7 +3,7 @@ package com.alas.dependancyInjecton.services;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Profile("EN")
+@Profile({"EN", "default"})
 @Service("I18nGreetingService")
 public class I18nEnglishGreetingService implements IGreetingService{
 
